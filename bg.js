83a4d1cea2b7d99b5b06c6a3970998aa63b776cd
Jackroyal <geqianst@gmail.com>
2014-11-17 00:29:11 +0800

    添加了响应快捷键的监听函数

--- bg.js
+++ bg.js
@@ -5,5 +5,16 @@ function checkUrl(tabId,info,tab) {
         bb.dispatch(tabId,info,tab);
 }
 
+function handleCommand(command){
+    console.log(command);
+    // chrome.tabs.getCurrent(function(tab){
+    //     console.log(tab);
+    //     chrome.tabs.executeScript(tab, {file: "kill.js"}, function() {
+    //     console.log('下线成功,如果要重新登录,你可能要刷新一下页面')});
+    // });
+
+}
 
 chrome.tabs.onUpdated.addListener(checkUrl);
+
+ chrome.commands.onCommand.addListener(handleCommand);
