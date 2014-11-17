d3daa4382fae64cdf2cba749caf2cbe6ccff5d03
Jackroyal <geqianst@gmail.com>
2014-11-18 01:31:48 +0800

    完善键盘快捷键的响应函数,添加消息机制,内容脚本和扩展之间的消息传递

--- js/logout.js
+++ js/logout.js
@@ -7,7 +7,7 @@ function lget(name) {
     return localStorage.getItem(name);
 }
 alert('999888888888888888888888888888888888');
-function pageOnunload() {
+function pageOnunload(wlan,ip) {
         alert('qqqqqqqqqqqqqqzzzzzzzzzzzzzzzzzzzzzzzzz');
     // try {
         var g_httpRequest;
@@ -15,7 +15,8 @@ function pageOnunload() {
             g_httpRequest = createHttpRequest();
         }
         g_httpRequest.abort();
-        g_httpRequest.open("GET", "http://120.202.164.10:8080/portal/servlets/LogoutServlet?"+lget('wlanacname')+"&"+lget('wlanuserip')+"&ssid=CMCC520&ATTRIBUTE_USERNAME=iWuhanFree2030&ATTRIBUTE_UUID=26FBE9A694B6221958CF6DE2704F0ECA&ATTRIBUTE_IPADDRESS="+lget('ip')+"&cancelAutomatismLogin=false", false);
+        g_httpRequest.open("GET", "http://120.202.164.10:8080/portal/servlets/LogoutServlet?"+wlan+"&"+ip+"&ssid=CMCC520&ATTRIBUTE_USERNAME=iWuhanFree2030&ATTRIBUTE_UUID=26FBE9A694B6221958CF6DE2704F0ECA&ATTRIBUTE_IPADDRESS="+lget('ip')+"&cancelAutomatismLogin=false", false);
+        console.log("http://120.202.164.10:8080/portal/servlets/LogoutServlet?"+wlan+"&"+ip+"&ssid=CMCC520&ATTRIBUTE_USERNAME=iWuhanFree2030&ATTRIBUTE_UUID=26FBE9A694B6221958CF6DE2704F0ECA&ATTRIBUTE_IPADDRESS="+lget('ip')+"&cancelAutomatismLogin=false");
         // alert("ATTRIBUTE_UUID");
         // alert("B963432C92AD20460E7080D38398224D");
         // alert("10.80.121.235");
@@ -40,4 +41,16 @@ function pageOnunload() {
     //     return null;
     // }
 }
-pageOnunload();
+/*
+    req:发送的消息内容
+    sen:发消息的扩展的id
+    senr:消息的回传函数(别名)
+ */
+chrome.extension.onMessage.addListener(function(req,sen,senr){
+    if(sen!=null){
+        pageOnunload(req['wlan'],req['wlanuserip']);
+    }
+    senr({'haha':'I am ok'});
+});
+
+//pageOnunload();
